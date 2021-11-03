16ba8d5cc710068cdaa259276f82c7e0f77ff5ea
OMyJIb <91065832+OMyJIb@users.noreply.github.com>
2021-11-04 03:55:21 +0800

    Add files via upload

---
+++ 12.7.3 Practice Shchepin_RG.py
@@ -0,0 +1,19 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+print("Процентные ставки по депозитам в банках следующие:", per_cent)
+
+a = int(input("Money"))
+
+tkb = 12*((a/100)*per_cent.get('ТКБ'))
+skb = 12*((a/100)*per_cent.get('СКБ'))
+vtb = 12*((a/100)*per_cent.get('ВТБ'))
+sber = 12*((a/100)*per_cent.get('СБЕР'))
+
+
+list_per_cent = []
+list_per_cent.append(tkb)
+list_per_cent.append(skb)
+list_per_cent.append(vtb)
+list_per_cent.append(sber)
+
+print("One-year deposit for every single bank", list_per_cent)
+print("Максимальная сумма, которую вы можете заработать", max(list_per_cent))
